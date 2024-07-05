7bfd3b831724053f54098ef386a5fb436d5806f6
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-07-05 11:09:07 +0200

    premiere session

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+   require_once __DIR__. "/templates/header.php";
+   require_once __DIR__. "/templates/carroussel.php";
+   require_once __DIR__. "/templates/home.php";
+
+?>
+
+
+
+
+
+
+
+    
+
+
+
+
+
+<?php 
+    require_once __DIR__. "/templates/footer.php";
+?>
